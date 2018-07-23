15693e7ff940db890f833e90296c3c72daa4d24b
Arturo Hernandez <arturohernandez@clear-launch.com>
2018-07-23 14:13:22 -0500

    Added irs provider list code

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -6,25 +6,26 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.irs.gov/e-file-providers/e-file-for-excise-tax-filers',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
-
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+		line = 0
+		lineResponse = {}
+        for provider_url in response.css("p a").extract():
+			line = line + 1
+		
+			item = {}
+			item["text"] = provider_url.css(" ::text").extract_first()
+			item["href"] = provider_url.css(" ::href").extract_first()
+			
+			
+			yield item
+			
+			if item["href"] == "https://www.i2290.com/index.aspx":
+				lineResponse["result"] = line
+				
+		yield lineResponse
+	
+	
+	
